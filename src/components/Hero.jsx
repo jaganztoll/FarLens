147cc66f369dfcc1875274374d17047df635c145
Julian Gruber <julian.a.e.gruber@gmail.com>
2025-05-16 03:33:08 +0200

    Update Hero component description

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ function Hero() {
   return (
     <section className="hero">
       <h1>Far Cry Real World Gallery</h1>
-      <p>Discover real locations behind the games.</p>
+      <p>Discover real locations behind the games. A Tribute.</p>
     </section>
   );
 }
