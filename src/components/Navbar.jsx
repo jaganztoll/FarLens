1ce790c7800ba40d633082c995c02a78a9de7f18
Julian Gruber <julian.a.e.gruber@gmail.com>
2025-05-20 21:21:27 +0200

    fix: Update Navbar links to close menu and scroll to top on click

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -122,22 +122,50 @@ function Navbar() {
 
               <ul className="nav-locations">
                 <li>
-                  <Link to="/fc3" className="fc3" onClick={closeMenu}>
+                  <Link
+                    to="/fc3"
+                    className="fc3"
+                    onClick={() => {
+                      setMenuOpen(false);
+                      setTimeout(() => window.scrollTo(0, 0), 0);
+                    }}
+                  >
                     Rook Islands
                   </Link>
                 </li>
                 <li>
-                  <Link to="/fc4" className="fc4" onClick={closeMenu}>
+                  <Link
+                    to="/fc4"
+                    className="fc4"
+                    onClick={() => {
+                      setMenuOpen(false);
+                      setTimeout(() => window.scrollTo(0, 0), 0);
+                    }}
+                  >
                     Kyrat
                   </Link>
                 </li>
                 <li>
-                  <Link to="/fc5" className="fc5" onClick={closeMenu}>
+                  <Link
+                    to="/fc5"
+                    className="fc5"
+                    onClick={() => {
+                      setMenuOpen(false);
+                      setTimeout(() => window.scrollTo(0, 0), 0);
+                    }}
+                  >
                     Hope County
                   </Link>
                 </li>
                 <li>
-                  <Link to="/fc6" className="fc6" onClick={closeMenu}>
+                  <Link
+                    to="/fc6"
+                    className="fc6"
+                    onClick={() => {
+                      setMenuOpen(false);
+                      setTimeout(() => window.scrollTo(0, 0), 0);
+                    }}
+                  >
                     Yara
                   </Link>
                 </li>
