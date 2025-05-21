945cc09e8596481b51bfeb7d98168c0d2b60957f
Julian Gruber <julian.a.e.gruber@gmail.com>
2025-05-22 01:24:46 +0200

    style: Adjust padding and layout for Credits page for better spacing and alignment

--- src/pages/Credits.jsx
+++ src/pages/Credits.jsx
@@ -2,7 +2,7 @@ import "../styles/Credits.css";
 
 function Credits() {
   return (
-    <main id="credits" className="credits">
+    <main id="credits" className="credits fullscreen">
       <h2>Photo Credits</h2>
       <p>
         The following photographers and creators provided the visuals that
@@ -15,93 +15,35 @@ function Credits() {
           <h3 className="fc3-credits">Rook Island</h3>
           <h4>Inspired by Indonesia</h4>
           <ul>
-            <li>
-              <strong>Alena Shekhovtcova</strong>
-            </li>
-            <li>
-              <strong>Aljhon Umlas</strong>
-            </li>
-            <li>
-              <strong>Andrew A</strong>
-            </li>
-            <li>
-              <strong>Anton Mislawsky</strong>
-            </li>
-            <li>
-              <strong>Ari Dinar</strong>
-            </li>
-            <li>
-              <strong>Benhur Leo</strong>
-            </li>
-            <li>
-              <strong>Candy Hartawan</strong>
-            </li>
-            <li>
-              <strong>Eswar M</strong>
-            </li>
-            <li>
-              <strong>Elliotm</strong>
-            </li>
-            <li>
-              <strong>Fotis Fotopoulos</strong>
-            </li>
-            <li>
-              <strong>Geio Tischler</strong>
-            </li>
-            <li>
-              <strong>Hayato Shin</strong>
-            </li>
-            <li>
-              <strong>Helen Ngoc N</strong>
-            </li>
-            <li>
-              <strong>Ion Ivan Sipilov</strong>
-            </li>
-            <li>
-              <strong>Josh Collesano</strong>
-            </li>
-            <li>
-              <strong>Joshua Kettle</strong>
-            </li>
-            <li>
-              <strong>Kaja Sariwating</strong>
-            </li>
-            <li>
-              <strong>Nhn</strong>
-            </li>
-            <li>
-              <strong>Paolo Nicolello</strong>
-            </li>
-            <li>
-              <strong>Rachel Salles</strong>
-            </li>
-            <li>
-              <strong>Rahmat Zumarli</strong>
-            </li>
-            <li>
-              <strong>Ranjith Alingal</strong>
-            </li>
-            <li>
-              <strong>Sam Balye</strong>
-            </li>
-            <li>
-              <strong>Silas Baisch</strong>
-            </li>
-            <li>
-              <strong>Simon Spring</strong>
-            </li>
-            <li>
-              <strong>Snapsaga</strong>
-            </li>
-            <li>
-              <strong>The Ian</strong>
-            </li>
-            <li>
-              <strong>Yh Zhou</strong>
-            </li>
-            <li>
-              <strong>Yulia Z</strong>
-            </li>
+            <li>Alena Shekhovtcova</li>
+            <li>Aljhon Umlas</li>
+            <li>Andrew A</li>
+            <li>Anton Mislawsky</li>
+            <li>Ari Dinar</li>
+            <li>Benhur Leo</li>
+            <li>Candy Hartawan</li>
+            <li>Eswar M</li>
+            <li>Elliotm</li>
+            <li>Fotis Fotopoulos</li>
+            <li>Geio Tischler</li>
+            <li>Hayato Shin</li>
+            <li>Helen Ngoc N</li>
+            <li>Ion Ivan Sipilov</li>
+            <li>Josh Collesano</li>
+            <li>Joshua Kettle</li>
+            <li>Kaja Sariwating</li>
+            <li>Nhn</li>
+            <li>Paolo Nicolello</li>
+            <li>Rachel Salles</li>
+            <li>Rahmat Zumarli</li>
+            <li>Ranjith Alingal</li>
+            <li>Sam Balye</li>
+            <li>Silas Baisch</li>
+            <li>Simon Spring</li>
+            <li>Snapsaga</li>
+            <li>The Ian</li>
+            <li>Yh Zhou</li>
+            <li>Yulia Z</li>
           </ul>
         </section>
 
@@ -109,105 +51,39 @@ function Credits() {
           <h3 className="fc4-credits">Kyrat</h3>
           <h4>Inspired by Nepal</h4>
           <ul>
-            <li>
-              <strong>Anup Shrestha</strong>
-            </li>
-            <li>
-              <strong>Azin Javadzadeh</strong>
-            </li>
-            <li>
-              <strong>Ben Pauer</strong>
-            </li>
-            <li>
-              <strong>Bikalpa Pokhrel</strong>
-            </li>
-            <li>
-              <strong>Danylo Istominov</strong>
-            </li>
-            <li>
-              <strong>Deepavali Gaind</strong>
-            </li>
-            <li>
-              <strong>Elton Sa</strong>
-            </li>
-            <li>
-              <strong>Fares Nimri</strong>
-            </li>
-            <li>
-              <strong>Giuseppe Mondi</strong>
-            </li>
-            <li>
-              <strong>Jochen Van Wylick</strong>
-            </li>
-            <li>
-              <strong>John T</strong>
-            </li>
-            <li>
-              <strong>Jonny James</strong>
-            </li>
-            <li>
-              <strong>Kabita Darlami</strong>
-            </li>
-            <li>
-              <strong>Kalle Kortelainen</strong>
-            </li>
-            <li>
-              <strong>Marc Linnemann</strong>
-            </li>
-            <li>
-              <strong>Martijn Vonk</strong>
-            </li>
-            <li>
-              <strong>Martin Skrivanek</strong>
-            </li>
-            <li>
-              <strong>Michael Starkie</strong>
-            </li>
-            <li>
-              <strong>Mike Blank</strong>
-            </li>
-            <li>
-              <strong>Olena Medvedieva</strong>
-            </li>
-            <li>
-              <strong>Prem Babu Moktan</strong>
-            </li>
-            <li>
-              <strong>Prijun Koirala</strong>
-            </li>
-            <li>
-              <strong>Pritush Munankarmi</strong>
-            </li>
-            <li>
-              <strong>Raimond Klavins</strong>
-            </li>
-            <li>
-              <strong>Sajan Rajbahak</strong>
-            </li>
-            <li>
-              <strong>Samrat Khadka</strong>
-            </li>
-            <li>
-              <strong>Sebastian Pena Lambarri</strong>
-            </li>
-            <li>
-              <strong>Shaouraav Sarose Shreshtha</strong>
-            </li>
-            <li>
-              <strong>Simon Berger</strong>
-            </li>
-            <li>
-              <strong>Slava Auchynnikau</strong>
-            </li>
-            <li>
-              <strong>Tim Van Kempen</strong>
-            </li>
-            <li>
-              <strong>V2osk</strong>
-            </li>
-            <li>
-              <strong>Yujesh Maleku</strong>
-            </li>
+            <li>Anup Shrestha</li>
+            <li>Azin Javadzadeh</li>
+            <li>Ben Pauer</li>
+            <li>Bikalpa Pokhrel</li>
+            <li>Danylo Istominov</li>
+            <li>Deepavali Gaind</li>
+            <li>Elton Sa</li>
+            <li>Fares Nimri</li>
+            <li>Giuseppe Mondi</li>
+            <li>Jochen Van Wylick</li>
+            <li>John T</li>
+            <li>Jonny James</li>
+            <li>Kabita Darlami</li>
+            <li>Kalle Kortelainen</li>
+            <li>Marc Linnemann</li>
+            <li>Martijn Vonk</li>
+            <li>Martin Skrivanek</li>
+            <li>Michael Starkie</li>
+            <li>Mike Blank</li>
+            <li>Olena Medvedieva</li>
+            <li>Prem Babu Moktan</li>
+            <li>Prijun Koirala</li>
+            <li>Pritush Munankarmi</li>
+            <li>Raimond Klavins</li>
+            <li>Sajan Rajbahak</li>
+            <li>Samrat Khadka</li>
+            <li>Sebastian Pena Lambarri</li>
+            <li>Shaouraav Sarose Shreshtha</li>
+            <li>Simon Berger</li>
+            <li>Slava Auchynnikau</li>
+            <li>Tim Van Kempen</li>
+            <li>V2osk</li>
+            <li>Yujesh Maleku</li>
           </ul>
         </section>
 
@@ -215,75 +91,29 @@ function Credits() {
           <h3 className="fc5-credits">Hope County</h3>
           <h4>Montana, USA</h4>
           <ul>
-            <li>
-              <strong>Alex Moliski</strong>
-            </li>
-            <li>
-              <strong>Aria W</strong>
-            </li>
-            <li>
-              <strong>Ben Owen</strong>
-            </li>
-            <li>
-              <strong>Bryn Gibson</strong>
-            </li>
-            <li>
-              <strong>Brynn Pedrick</strong>
-            </li>
-            <li>
-              <strong>Colin Maynard</strong>
-            </li>
-            <li>
-              <strong>David Morris</strong>
-            </li>
-            <li>
-              <strong>Dylan Taylor</strong>
-            </li>
-            <li>
-              <strong>Gordon Lewis</strong>
-            </li>
-            <li>
-              <strong>Maddy Baker</strong>
-            </li>
-            <li>
-              <strong>Matthew Lancaster</strong>
-            </li>
-            <li>
-              <strong>Mick Kirchman</strong>
-            </li>
-            <li>
-              <strong>Mikel Ibarluzea</strong>
-            </li>
-            <li>
-              <strong>Nate Foong</strong>
-            </li>
-            <li>
-              <strong>Nils Huenerfuerst</strong>
-            </li>
-            <li>
-              <strong>Pete Alexopoulos</strong>
-            </li>
-            <li>
-              <strong>Roger Lipera</strong>
-            </li>
-            <li>
-              <strong>Stephen Pedersen</strong>
-            </li>
-            <li>
-              <strong>Steven Cordes</strong>
-            </li>
-            <li>
-              <strong>Tevin Trinh</strong>
-            </li>
-            <li>
-              <strong>Todd Schlender</strong>
-            </li>
-            <li>
-              <strong>Troy Olson</strong>
-            </li>
-            <li>
-              <strong>Yohan Marion</strong>
-            </li>
+            <li>Alex Moliski</li>
+            <li>Aria W</li>
+            <li>Ben Owen</li>
+            <li>Bryn Gibson</li>
+            <li>Brynn Pedrick</li>
+            <li>Colin Maynard</li>
+            <li>David Morris</li>
+            <li>Dylan Taylor</li>
+            <li>Gordon Lewis</li>
+            <li>Maddy Baker</li>
+            <li>Matthew Lancaster</li>
+            <li>Mick Kirchman</li>
+            <li>Mikel Ibarluzea</li>
+            <li>Nate Foong</li>
+            <li>Nils Huenerfuerst</li>
+            <li>Pete Alexopoulos</li>
+            <li>Roger Lipera</li>
+            <li>Stephen Pedersen</li>
+            <li>Steven Cordes</li>
+            <li>Tevin Trinh</li>
+            <li>Todd Schlender</li>
+            <li>Troy Olson</li>
+            <li>Yohan Marion</li>
           </ul>
         </section>
 
@@ -291,87 +121,33 @@ function Credits() {
           <h3 className="fc6-credits">Yara</h3>
           <h4>Inspired by Cuba</h4>
           <ul>
-            <li>
-              <strong>Alex Meier</strong>
-            </li>
-            <li>
-              <strong>Alex Plesovskich</strong>
-            </li>
-            <li>
-              <strong>Alexander Kunze</strong>
-            </li>
-            <li>
-              <strong>AXP Photography</strong>
-            </li>
-            <li>
-              <strong>Daniel Sessler</strong>
-            </li>
-            <li>
-              <strong>Dariel Diaz</strong>
-            </li>
-            <li>
-              <strong>Diego Van Sommeren</strong>
-            </li>
-            <li>
-              <strong>Emily Crawford</strong>
-            </li>
-            <li>
-              <strong>Eric Ward</strong>
-            </li>
-            <li>
-              <strong>Falco Negenman</strong>
-            </li>
-            <li>
-              <strong>Florian Wehde</strong>
-            </li>
-            <li>
-              <strong>Gayatri Malhotra</strong>
-            </li>
-            <li>
-              <strong>Isaac Gallinati</strong>
-            </li>
-            <li>
-              <strong>Jason Gamble</strong>
-            </li>
-            <li>
-              <strong>Jennifer Chen</strong>
-            </li>
-            <li>
-              <strong>JF Martin</strong>
-            </li>
-            <li>
-              <strong>Jonny Wardle</strong>
-            </li>
-            <li>
-              <strong>Max Letek</strong>
-            </li>
-            <li>
-              <strong>Nathan Dumlao</strong>
-            </li>
-            <li>
-              <strong>Michael Schultze</strong>
-            </li>
-            <li>
-              <strong>Pavel</strong>
-            </li>
-            <li>
-              <strong>Remy Gieling</strong>
-            </li>
-            <li>
-              <strong>Ricardo IV Tamayo</strong>
-            </li>
-            <li>
-              <strong>Robin Canfield</strong>
-            </li>
-            <li>
-              <strong>Tiago Claro</strong>
-            </li>
-            <li>
-              <strong>Tim Stief</strong>
-            </li>
-            <li>
-              <strong>Wolf Schram</strong>
-            </li>
+            <li>Alex Meier</li>
+            <li>Alex Plesovskich</li>
+            <li>Alexander Kunze</li>
+            <li>AXP Photography</li>
+            <li>Daniel Sessler</li>
+            <li>Dariel Diaz</li>
+            <li>Diego Van Sommeren</li>
+            <li>Emily Crawford</li>
+            <li>Eric Ward</li>
+            <li>Falco Negenman</li>
+            <li>Florian Wehde</li>
+            <li>Gayatri Malhotra</li>
+            <li>Isaac Gallinati</li>
+            <li>Jason Gamble</li>
+            <li>Jennifer Chen</li>
+            <li>JF Martin</li>
+            <li>Jonny Wardle</li>
+            <li>Max Letek</li>
+            <li>Nathan Dumlao</li>
+            <li>Michael Schultze</li>
+            <li>Pavel</li>
+            <li>Remy Gieling</li>
+            <li>Ricardo IV Tamayo</li>
+            <li>Robin Canfield</li>
+            <li>Tiago Claro</li>
+            <li>Tim Stief</li>
+            <li>Wolf Schram</li>
           </ul>
         </section>
       </div>
