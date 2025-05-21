1cd137c47c0178caa8a08b50250e9f455da9e229
Julian Gruber <julian.a.e.gruber@gmail.com>
2025-05-21 09:48:55 +0200

    feat: Add Credits page and link in Footer for photo credits

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import FC3Gallery from "./pages/FC3Gallery";
 import FC4Gallery from "./pages/FC4Gallery";
 import FC5Gallery from "./pages/FC5Gallery";
 import FC6Gallery from "./pages/FC6Gallery";
+import Credits from "./pages/Credits";
 
 function App() {
   return (
@@ -17,15 +18,18 @@ function App() {
       <Navbar />
       <main>
         <Routes>
-          {/* Startseite mit Hero, About, Gallery and Contact */}
+          {/* Hero, About, Gallery */}
           <Route path="/" element={<HomePage />} />
-          {/* Far Cry Galerien */}
+          {/* Far Cry 3-6 */}
           <Route path="/fc3" element={<FC3Gallery />} />
           <Route path="/fc4" element={<FC4Gallery />} />
           <Route path="/fc5" element={<FC5Gallery />} />
           <Route path="/fc6" element={<FC6Gallery />} />
+          {/* Credits */}
+          <Route path="/credits" element={<Credits />} />
         </Routes>
       </main>
+      {/* Footer */}
       <Footer />
     </Router>
   );
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import "../styles/Footer.css";
+import { Link } from "react-router-dom"; // Nur wenn du React Router verwendest
 
 function Footer() {
   return (
@@ -6,6 +7,9 @@ function Footer() {
       <p>
         &copy; {new Date().getFullYear()} FarLens – Photography inspired by Far
         Cry
+      </p>
+      <p className="credits-link">
+        <Link to="/credits">Behind the Lens</Link>
       </p>
     </footer>
   );
